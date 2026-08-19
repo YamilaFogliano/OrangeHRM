1ad1f02b13efecf6144f4422cbea7f3952f35456
YamilaFogliano <yamilafogliano@hotmail.com>
2026-08-19 12:30:46 -0300

    Validacion de menu lateral

---
+++ tests/navigation.spec.ts
@@ -0,0 +1,48 @@
+import { test, expect } from '@playwright/test';
+
+test('Validación de menús laterales', async ({ page }) => {
+
+    console.log('✅ Ingresando credenciales');
+
+    await page.goto('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login');
+
+    await page.getByRole('textbox', { name: 'Username' }).fill('Admin');
+    await page.getByRole('textbox', { name: 'Password' }).fill('admin123');
+    await page.getByRole('button', { name: 'Login' }).click();
+
+    await expect(page.getByRole('link', { name: 'Dashboard' })).toBeVisible();
+
+    console.log('✅ Ingreso exitoso');
+
+    const leftMenuITems = page.getByLabel('Sidepanel').getByRole('listitem')
+    const currentMenuItemsCount = await leftMenuITems.count()
+    console.log('Current menu items count', currentMenuItemsCount)
+
+    const currentMenuItems: string[] = []
+
+    for (let i = 0; i < currentMenuItemsCount; i++) {
+
+        const menuText = await leftMenuITems.nth(i).innerText()
+        currentMenuItems.push(menuText)
+    }
+
+    console.log(currentMenuItems)
+
+    const expectedMenuItems = [
+        'Admin',
+        'PIM',
+        'Leave',
+        'Time',
+        'Recruitment',
+        'My Info',
+        'Performance',
+        'Dashboard',
+        'Directory',
+        'Maintenance',
+        'Claim',
+        'Buzz'
+
+    ];
+
+    expect(currentMenuItems).toEqual(expectedMenuItems)
+});
