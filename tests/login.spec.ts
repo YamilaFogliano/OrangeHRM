37ebde96189d90935306936830d0653eeab2724d
YamilaFogliano <yamilafogliano@hotmail.com>
2026-08-19 11:48:48 -0300

    Captura de usuarios en tabla

--- tests/login.spec.ts
+++ tests/login.spec.ts
@@ -1,20 +1,17 @@
 import { test, expect } from '@playwright/test';
 
-test.describe("Suite de tests", async () => {
 
-    test('Login de Orange HRM', async ({ page }) => {
+test('Login de Orange HRM', async ({ page }) => {
 
-        console.log('✅ Ingresando credenciales');
+    console.log('✅ Ingresando credenciales');
 
-        await page.goto('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login');
+    await page.goto('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login');
 
-        await page.getByRole('textbox', { name: 'Username' }).fill('Admin');
-        await page.getByRole('textbox', { name: 'Password' }).fill('admin123');
-        await page.getByRole('button', { name: 'Login' }).click();
+    await page.getByRole('textbox', { name: 'Username' }).fill('Admin');
+    await page.getByRole('textbox', { name: 'Password' }).fill('admin123');
+    await page.getByRole('button', { name: 'Login' }).click();
 
-        await expect(page.getByRole('link', { name: 'Dashboard' })).toBeVisible();
+    await expect(page.getByRole('link', { name: 'Dashboard' })).toBeVisible();
 
-        console.log('✅ Ingreso exitoso');
-    });
-
+    console.log('✅ Ingreso exitoso');
 });
---
+++ tests/user.spec.ts
@@ -0,0 +1,36 @@
+import { test, expect } from '@playwright/test';
+
+test('Login y captura de usuarios den Orange HRM', async ({ page }) => {
+
+    console.log('✅ Ingresando credenciales');
+    await page.goto('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login');
+    await page.getByRole('textbox', { name: 'Username' }).fill('Admin');
+    await page.getByRole('textbox', { name: 'Password' }).fill('admin123');
+    await page.getByRole('button', { name: 'Login' }).click();
+    await expect(page.getByRole('link', { name: 'Admin' })).toBeVisible();
+    console.log('✅ Ingreso exitoso');
+
+    console.log('✅ Recolectando usuarios');
+    await page.getByRole('link', { name: 'Admin' }).click();
+
+    await page.getByRole('navigation', { name: 'Topbar menu' }).getByText('User Management').click();
+    await page.getByRole('menuitem', { name: 'Users' }).click();
+
+    const rows = page.getByRole('table').getByRole('row')
+    const usernames: string[] = []
+
+    const rowCount = await rows.count()
+
+    for (let i = 1; i < rowCount; i++) {
+
+        const cell = rows.nth(i).getByRole('cell').nth(1)
+        const username = await cell.textContent()
+
+        if (username) {
+            usernames.push(username)
+        }
+    }
+
+    console.log(usernames)
+
+});
